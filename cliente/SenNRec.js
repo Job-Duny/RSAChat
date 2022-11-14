40c32c7f0b706be5452ec5262cff9edb777e0a82
Job-Duny <gpinam2000@alumno.ipn.mx>
2022-11-14 01:45:53 -0600

    sex is relative

--- cliente/SenNRec.js
+++ cliente/SenNRec.js
@@ -1,110 +1,106 @@
-import bigInt from 'big-integer';
-import RSA from './rsa.js';
+const keys = RSA.generate(512);
 
-const keys = RSA.generate(1024);
+        const enc = (msg) => {
+        const encoded_message = RSA.encode(msg);
+        console.log("Encoded message:", encoded_message);
+        const encrypted_message = RSA.encrypt(encoded_message, keys.n, keys.e);
+        console.log("Encrypted message:", encrypted_message);
+        return encrypted_message;
+        };
 
-const enc = (mess) => {
+        const dec = (data) => {
+        console.log("Encrypted message:", data.msgEnc);
+        const decrypted_message = RSA.decrypt(
+            bigInt(data.msgEnc),
+            data.d,
+            data.n
+        );
 
-    // code n encrypt message
+        console.log("Keys");
+        console.log("n:", keys.n.toString());
+        console.log("d:", keys.d.toString());
+        console.log("e:", keys.e.toString());
+        console.log("Decrypted message:", decrypted_message);
+        const decoded_message = RSA.decode(decrypted_message);
+        console.log("Decoded message:", decoded_message.toString());
+        return decoded_message.toString();
+        };
 
-    const encoded_message = RSA.encode(mess); // se sustitullle x mess 
-    console.log("Encoded: ",encoded_message);
-    const encrypted_message = RSA.encrypt(encoded_message, keys.e, keys.n);
-    console.log("Encrypted: ",encrypted_message);
+        const socket = io();
+        const chatMain = document.getElementById("chatMain");
+        const username = document.getElementById("username");
+        const message = document.getElementById("message");
+        const notification = document.getElementById("notification");
+        const form = document.querySelector("form");
 
-    return encrypted_message;
-}
-
-const dec = (data) => {
-
-    // decrypt and decode message
-
-    console.log(data.msgEnc);
-    const decrypted_message = RSA.decrypt(bigInt(data.msgEnc), keys.d, keys.n);
-
-    console.log('keys:');
-    console.log(keys.n.toString());
-    console.log(keys.e.toString());
-    console.log(keys.d.toString());
-
-
-    console.log("Decripted: ", decrypted_message);
-    const decoded_message = RSA.decode(decrypted_message);
-    console.log("Decoded: ", decoded_message);
-
-    return decoded_message;
-}
-
-const socket = io();
-const chatMain = document.getElementById('groupMain');
-const user = document.getElementById('user');
-const message = document.getElementById('mess');
-const notif = document.getElementById('notif');
-const form = document.querySelector('form');
-
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    if(user.value && message.value){
-        console.log(message.value);
-        console.log(message.value.length);
-        if(message.value.length < 76){
+        form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        if (username.value && message.value ) {
+            console.log(message.value)
+            console.log(message.value.length)
+            if(message.value.length<76){
             const msgEnc = enc(message.value);
-            socket.emit('chat-message', {message: { msgEnc, d: keys.d, n: keys.n }, user: user.value});
-            user.disabled = true;
-
-            const li = document.createElement('li');
-            li.cllassName = 'sent-me';
-            li.innerHTML = `<strong>Me</strong>: ${dec({msgEnc, d: keys.d, n: keys.n})}`;
+            socket.emit("chat-message", {
+                username: username.value,
+                message: { msgEnc, d: keys.d, n: keys.n },
+            });
+            username.disabled = true;
+            const li = document.createElement("li");
+            li.className = "me";
+            li.innerHTML = `<strong>Me</strong>: ${dec({
+                msgEnc,
+                d: keys.d,
+                n: keys.n,
+            })}`;
             chatMain.appendChild(li);
-            message.value = '';
-        }else{
-            notif.innerHTML = 'Message too long'
-        } 
-    }else{
-        notif.innerHTML = 'Please fill all fields'
-    }
-});
+            window.scrollTo(0, document.body.scrollHeight);
+            message.value = "";
+            }else{
+            alert("Message lenght must be less than 76 char")
+            }
+        } else {
+            alert("Username and message are required");
+        }
+        });
 
-user.addEventListener('input', (e) => {
-    if (user.value) {
-        message.disabled = false;
-    } else {
-        message.disabled = true;
-    }
-});
+        username.addEventListener("input", (e) => {
+        if (username.value) {
+            message.disabled = false;
+        } else {
+            message.disabled = true;
+        }
+        });
 
-message.addEventListener('keydown', () => {
-    socket.emit('typing', user.value);
-});
+        message.addEventListener("input", () => {
+        socket.emit("typing", username.value);
+        });
 
-socket.on('chat-message', (data) => {
-    const li = document.createElement('li');
-    console.log(data);
-    const decMess = dec(data.message);
-    li.innerHTML = `<strong>${data.user}</strong>: ${decMess}`;
-    chatMain.appendChild(li);
-    window.scrollTo(0, document.body.scrollHeight);
-});
-
-socket.on('user-disconnected', (data) => {
-    const li = document.createElement('li');
-    li.className = 'notif';
-    li.innerHTML = `<strong>${data.user}</strong> disconnected`;
-    chatMain.appendChild(li);
-    window.scrollTo(0, document.body.scrollHeight);
-});
-
-socket.on('user-connected', (data) => {
-    const li = document.createElement('li');
-    li.className = 'notif';
-    li.innerHTML = `<strong>${data.user}</strong> connected`;
-    chatMain.appendChild(li);
-    window.scrollTo(0, document.body.scrollHeight);
-});
-
-socket.on('typing', (data) => {
-    notif.innerHTML = `<strong>${data}</strong> is typing...`;
-    setTimeout(() => {
-        notif.innerHTML = '';
-    }, 1000);
-});
+        socket.on("chat-message", (data) => {
+        const li = document.createElement("li");
+        li.className = "entry";
+        console.log(data);
+        const decMsg = dec(data.message);
+        li.innerHTML = `<strong>${data.username}</strong>: ${decMsg}`;
+        chatMain.appendChild(li);
+        window.scrollTo(0, document.body.scrollHeight);
+        });
+        socket.on("user-disconnected", (data) => {
+        const li = document.createElement("li");
+        li.className = "notify";
+        li.innerHTML = `<strong>${data}</strong>`;
+        chatMain.appendChild(li);
+        window.scrollTo(0, document.body.scrollHeight);
+        });
+        socket.on("user-connected", (data) => {
+        const li = document.createElement("li");
+        li.className = "notify";
+        li.innerHTML = `<strong>${data}</strong>`;
+        chatMain.appendChild(li);
+        window.scrollTo(0, document.body.scrollHeight);
+        });
+        socket.on("typing", (data) => {
+        notification.innerHTML = `<strong>${data}</strong> is typing...`;
+        setTimeout(() => {
+            notification.innerHTML = "";
+        }, 2000);
+        });
--- cliente/rsa.js
+++ cliente/rsa.js
@@ -2,60 +2,73 @@
 
 class RSA {
     static randomPrime(bits) {
-        const min = bigInt(2).pow(bits - 1);
-        const max = bigInt(2).pow(bits).prev();
+        const min = bigInt.one.shiftLeft(bits - 1);
+        const max = bigInt.one.shiftLeft(bits).prev();
 
         while (true) {
-            let p = bigInt.randBetween(min, max);
-            if (p.isProbablePrime(256)) {
-                return p;
-            }
-        }    
-    }
-
-    static generate(keysize){
-        const e = bigInt(65537);
-        let p, q, totient;
-
-        do{
-            p = this.randomPrime(keysize / 2 );
-            q = this.randomPrime(keysize / 2 );
-            totient = bigInt.lcm(p.prev(), q.prev());
-        } while (bigInt.gcd(e, totient).notEquals(1) || p.minus(q).abs().shiftRight(keysize / 2 - 100).isZero());
-        
-        return {
-            e,
-            n: p.multiply(q),
-            d: e.modInv(totient)
+        let p = bigInt.randBetween(min, max);
+        if (p.isProbablePrime(256)) {
+            return p;
+        }
         }
     }
 
-    static encrypt(enodeMess, e, n){
-        return bigInt(enodeMess).modPow(e, n);
+    static generate(keysize) {
+        const e = bigInt(65537);
+        let p;
+        let q;
+        let totient;
+
+        do {
+        p = this.randomPrime(keysize / 2);
+        q = this.randomPrime(keysize / 2);
+        totient = bigInt.lcm(p.prev(), q.prev());
+        } while (
+        bigInt.gcd(e, totient).notEquals(1) ||
+        p
+            .minus(q)
+            .abs()
+            .shiftRight(keysize / 2 - 100)
+            .isZero()
+        );
+
+        return {
+        e,
+        n: p.multiply(q),
+        d: e.modInv(totient),
+        };
     }
 
-    static decrypt(encryptedMess, d, n){
-        return bigInt(encryptedMess).modPow(d, n);
+    static encrypt(encodedMsg, n, e) {
+        return bigInt(encodedMsg).modPow(e, n);
     }
 
-    static encode(str){
-        const code = str.split('').map(c => c.charCodeAt()).join('');
-        return bigInt(code);
+    static decrypt(encryptedMsg, d, n) {
+        return bigInt(encryptedMsg).modPow(d, n);
     }
 
-    static decode(code){
+    static encode(str) {
+        const codes = str
+        .split("")
+        .map((i) => i.charCodeAt())
+        .join("");
+
+        return bigInt(codes);
+    }
+
+    static decode(code) {
         const stringified = code.toString();
-        let string = '';
+        let string = "";
 
         for (let i = 0; i < stringified.length; i += 2) {
-            let num = Number(stringified.substr(i, 2));
+        let num = Number(stringified.substr(i, 2));
 
-            if (num <= 30) {
-                string += String.fromCharCode(Number(stringified.substr(i, 3)));
-                i++;
-            } else {
-                string += String.fromCharCode(num);
-            }    
+        if (num <= 30) {
+            string += String.fromCharCode(Number(stringified.substr(i, 3)));
+            i++;
+        } else {
+            string += String.fromCharCode(num);
+        }
         }
 
         return string;
